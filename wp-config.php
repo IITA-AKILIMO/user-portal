39c4614a088eb013ca77f167c49e3738c528440e
AKILIMO prod server <s.barasa@cgiar.org>
2023-01-05 18:31:41 +0000

    refactor: updated config

--- wp-config.php
+++ wp-config.php
@@ -26,16 +26,19 @@ define( 'DB_NAME', 'akilimo_portal' );
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'andalite6' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+
+/** force direct method instead of FTP **/
+define('FS_METHOD','direct');
 
 /**#@+
  * Authentication unique keys and salts.
