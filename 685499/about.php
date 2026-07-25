fa8a2f10d8655ac8ae716f96786d84efd887eda0
akilimo-vm <akilimo@akilimo-vm.lse5crnmzqmuvpjanafo4p0gub.bx.internal.cloudapp.net>
2026-07-25 06:38:06 +0000

    Update 4 files (4 files changed, 55 insertions(+)): 685499/1.txt,685499/about.php 685499/admin.php,685499/index.php

---
+++ 685499/about.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>vo4.xyz</title>
+<meta name="description" content="Find information, resources and relevant links for vo4.xyz. This domain may be for sale.">
+<script async src="https://assets.abovedomains.com/javascript/forsale.min.js?d=vo4.xyz"></script>
+<style>body{background:#101c36;color:#fff;font-family:Helvetica,Arial,sans-serif;margin:0}h1{text-align:center;margin-top:1rem}.content{max-width:640px;margin:1rem auto;padding:0 1rem;text-align:center}.footer{text-align:center;padding:1.5rem 1rem;font-size:.85rem}.footer a{color:#fff}</style>
+</head>
+<body>
+<h1>vo4.xyz</h1>
+<div class="content">Find information, resources and relevant links for vo4.xyz.<br/>This domain may be for sale.</div>
+<div class="footer">
+<a href="/terms.html">Terms of Service</a> <span>|</span> <a href="/privacy.html">Privacy Policy</a>
+</div>
+</body>
+</html>
